02df1e2e6169e7c9993a4f24cf18e02c2d0bca69
Neha Sinha <85924063+nehaassinha@users.noreply.github.com>
2021-09-08 10:04:12 +0530

    Updated App.js
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
                   Reports</li>
               </ul>
               <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <button class="btn btn-primary text-uppercase">Button</button>
               </div>
             </div>
           </div>
@@ -52,7 +52,7 @@ function App() {
                   Reports</li>
               </ul>
               <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <button class="btn btn-primary text-uppercase">Button</button>
               </div>
             </div>
           </div>
@@ -77,7 +77,7 @@ function App() {
                 <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
               </ul>
               <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+                <button class="btn btn-primary text-uppercase">Button</button>
               </div>
             </div>
           </div>
